fb2f57e73d699bcba88813e66e0873ad2b1ec1c9
Mishrule <Mishrule1@gmail.com>
2022-05-31 21:43:21 +0000

    wip

--- BookStoreAppNET5.API/Startup.cs
+++ BookStoreAppNET5.API/Startup.cs
@@ -1,3 +1,4 @@
+using BookStoreAppNET5.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,8 +28,15 @@ namespace BookStoreAppNET5.API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-
+      services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("DefaultConnection")));
+      services.AddCors(o =>
+      {
+        o.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+      });
       services.AddControllers();
+
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookStoreAppNET5.API", Version = "v1" });
@@ -45,6 +54,8 @@ namespace BookStoreAppNET5.API
       }
 
       app.UseHttpsRedirection();
+
+      app.UseCors("CorsPolicy");
 
       app.UseRouting();
 
